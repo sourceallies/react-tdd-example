2f68bd0688f684a6fd268c514a4203448809c15a
Michael Parker <mparker@sourceallies.com>
2015-08-21 15:56:46 -0500

    Passing: Add ability to create list items

--- components/todoList.js
+++ components/todoList.js
@@ -1,15 +1,29 @@
 var React = require('react/addons');
 
 var TodoList = React.createClass({
+	getInitialState : function () {
+		return ({items : []});
+	},
+	add : function () {
+		var text = React.findDOMNode(this.refs.textField).value;
+		var items = this.state.items;
+		items.push(text);
+		this.setState({items : items});
+	},
 	render : function () {
+		var items = this.state.items.map(function (item, i) {
+			return (
+				<li key={i}>{item}</li>
+			);
+		});
 		return (
 			<div>
 				<h1>To-do List</h1>
 				<br/>
-				<ul></ul>
+				<ul>{items}</ul>
 				<br/>
-				<input type='text'/>
-				<button>Add item to list</button>	
+				<input type='text' ref='textField'/>
+				<button onClick={this.add} ref='button'>Add item to list</button>	
 			</div>
 		);
 	}
