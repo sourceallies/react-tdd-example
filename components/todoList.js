3f2c71e58d1c59484c59243aadb56d7e6f47e631
Michael Parker <mparker@sourceallies.com>
2015-08-21 15:33:36 -0500

    Passing: Add DOM elements to TodoList

--- components/todoList.js
+++ components/todoList.js
@@ -2,7 +2,16 @@ var React = require('react/addons');
 
 var TodoList = React.createClass({
 	render : function () {
-		return <div>Hello, World!</div>
+		return (
+			<div>
+				<h1>To-do List</h1>
+				<br/>
+				<ul></ul>
+				<br/>
+				<input type='text'/>
+				<button>Add item to list</button>	
+			</div>
+		);
 	}
 });
 
