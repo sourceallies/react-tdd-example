5415db24af1ee61c2005bb3b4a313a06abc0a969
Michael Parker <mparker@sourceallies.com>
2015-08-21 15:29:47 -0500

    Failing: Add DOM elements to TodoList

--- tests/todoList-test.js
+++ tests/todoList-test.js
@@ -9,4 +9,28 @@ describe('TodoList', function () {
 		expect(todoList).toBeTruthy();
 	});
 
+	it('contains one un-ordered list', function () {
+		var todoList = TestUtils.renderIntoDocument(<TodoList/>);
+		var unorderedLists = TestUtils.scryRenderedDOMComponentsWithTag(todoList, 'ul');
+		expect(unorderedLists.length).toEqual(1);
+	});
+
+	it('contains one header', function () {
+		var todoList = TestUtils.renderIntoDocument(<TodoList/>);
+		var headers = TestUtils.scryRenderedDOMComponentsWithTag(todoList, 'h1');
+		expect(headers.length).toEqual(1);
+	});
+
+	it('contains an input for naming a new list item', function () {
+		var todoList = TestUtils.renderIntoDocument(<TodoList/>);
+		var inputs = TestUtils.scryRenderedDOMComponentsWithTag(todoList, 'input');
+		expect(inputs.length).toEqual(1);
+	});
+
+	it('contains a button for adding a new list item', function () {
+		var todoList = TestUtils.renderIntoDocument(<TodoList/>);
+		var buttons = TestUtils.scryRenderedDOMComponentsWithTag(todoList, 'button');
+		expect(buttons.length).toEqual(1);
+	});
+
 });
